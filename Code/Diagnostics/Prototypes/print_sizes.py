b279a0dfd6c53b5ae835d95a4267e65677dfdb5e
Tom Rice <t.rice90@gmail.com>
2020-07-06 17:36:59 -0400

    format changes and the final functionality: printing load times and actual table lengths

--- Code/Diagnostics/Prototypes/print_sizes.py
+++ Code/Diagnostics/Prototypes/print_sizes.py
@@ -3,6 +3,8 @@
 
 import os
 import glob
+import time
+import astropy.table as tab
 
 data_location = os.path.expanduser(
     "~/Documents/Variability_Project_2020/Data/Raw_Downloads/"
@@ -12,21 +14,27 @@ data_files = glob.glob(data_location + "*.fits.gz")
 
 # zero-order sanity check: print the filenames with their paths.
 
-print ("Here are the filenames: ")
+print("\n  Here are the filenames: ")
 for filename in data_files:
     print(filename)
-print("")
 
-data_files_without_prefix = [ x[len(data_location):] for x in data_files]
+data_files_without_prefix = [x[len(data_location) :] for x in data_files]
 
 # first, sanity check, get the actual filesizes in bytes
-print("Here are their sizes, in MB:")
+print("\n  Here are their sizes, in MB:")
 for filename, filename_sans_prefix in zip(data_files, data_files_without_prefix):
     print(f"{filename_sans_prefix}: {os.path.getsize(filename) / 1e6:.1f} MB")
 
-# # now, load each file, print its size (as a length), then close it
-# for filename in data_files:
+# now, load each file, print its size (its length), then close it
 
-# 	# this `tab` variable is going to be overwritten in every iteration of the loop
-# 	tab =
-#     print(os.path.getsize(filename))
+print("\n  Load times and table sizes, in rows:")
+for filename, filename_sans_prefix in zip(data_files, data_files_without_prefix):
+    start_time = time.time()
+
+    # this `table` variable is going to be overwritten in every iteration of the loop
+    table = tab.Table.read(filename, format="fits")
+    elapsed_time = time.time() - start_time
+
+    print(f"{filename_sans_prefix} took {elapsed_time:.2f} s to load")
+    print(f"{filename_sans_prefix} has {len(table)} rows ({len(table)/1e6:.1f}M rows)")
+    print("")
