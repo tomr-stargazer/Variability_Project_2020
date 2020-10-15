1221d2b415293e96a8a06ebae497bb2c55e7a252
Tom Rice <t.rice90@gmail.com>
2020-10-15 11:57:47 -0400

    most of the logistical functionality is built

--- wuvars/analysis/create_summary_spreadsheets.py
+++ wuvars/analysis/create_summary_spreadsheets.py
@@ -1,7 +1,10 @@
 # For each graded_cleaned_scrubbed_clipped_whatever...
 # run variability_selection.spreadsheet_maker
 
+import os
+import pathlib
 from wuvars.analysis.variability_selection import spreadsheet_maker
+
 
 def write_summary_spreadsheet(filename, output):
     # takes in a dataset from a given filename
@@ -9,15 +12,32 @@ def write_summary_spreadsheet(filename, output):
     # writes that to file
     # returns nothing
 
+    # load in the .fits.gz file - either via astropy or directly into pandas
+
     pass
 
 
 if __name__ == "__main__":
-    # do some globbing? didn't we do something like this before?
-    # grab all of the things and then 
-    input_filenames = []
-    output_filenames = []
 
-    for in_file, out_file in zip(input_filenames, output_filenames):
+    wserv_ids = [1, 5, 7, 8, 11]
 
-        write_summary_spreadsheet(in_file, out_file)
+    input_root = "/Users/tsrice/Documents/Variability_Project_2020/wuvars/Data/reduction_artifacts/"
+    output_root = "/Users/tsrice/Documents/Variability_Project_2020/wuvars/Data/analysis_artifacts"
+
+    for wserv in wserv_ids:
+        input_path = os.path.join(
+            input_root,
+            f"wserv{str(wserv)}",
+            f"WSERV{str(wserv)}_graded_clipped0.95_scrubbed0.1_dusted0.5.h5",
+        )
+
+        output_path = os.path.join(
+            output_root,
+            f"wserv{str(wserv)}",
+            f"WSERV{str(wserv)}_graded_clipped0.95_scrubbed0.1_dusted0.5_summary_spreadsheet.h5",
+        )
+
+        pathlib.Path(output_path).mkdir(parents=True, exist_ok=True)
+
+        print(input_path, output_path)
+        write_summary_spreadsheet(input_path, output_path)
