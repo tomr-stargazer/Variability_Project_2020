3751e5a8bfad33ed1c8823e3606a98c9d1102bb6
Tom Rice <t.rice90@gmail.com>
2020-10-22 13:55:05 -0400

    some diagnostics on source maps - this has revealed an aberration in wserv5 that i seem to have long forgotten, and need to deal with before cleaning again

---
+++ wuvars/diagnostics/source_maps.py
@@ -0,0 +1,10 @@
+"""
+Let's make maps and plots to show where the sources are and when they were observed.
+
+Goals:
+- RA / Dec. Regular old maps. 
+- RA / time, Dec / time: showing the spatial coverage as a function of time.
+
+"""
+
+def 
