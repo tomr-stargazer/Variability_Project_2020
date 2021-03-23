3f515ca3c9f830fdf0a042637da772dc785c6295
Tom Rice <t.rice90@gmail.com>
2021-03-23 16:08:35 -0400

    starting something here - it's still empty

---
+++ wuvars/plotting/triptych.py
@@ -0,0 +1,14 @@
+"""
+I'm making a set of triple-plots regarding brown dwarf variables.
+
+"""
+
+# Let's prototype this in a Jupyter notebook.
+
+# left hand: map of variables, with Q0 underneath
+# middle: map of "stars with BD-compatible position in color-mag space", with Q0 underneath
+# right: map of variable BD candidates, with Q0 underneath and possibly also gently flagging BDs and vars.
+
+# I think red: variable, blue: BD cand, purple: both is a clear visual cue.
+
+def 
