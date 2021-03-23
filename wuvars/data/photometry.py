bc6a23ffefe612695003ee1f2ea114aedf000044
Tom Rice <t.rice90@gmail.com>
2021-03-23 16:48:55 -0400

    refactored approach to data loading. still need to refactor references to wuvars.data

--- wuvars/data/photometry.py
+++ wuvars/data/photometry.py
@@ -17,23 +17,78 @@ from wuvars.analysis.variability_selection import data_nuller
 
 wserv_ids = [1, 5, 7, 8, 11]
 
+wserv_ids_v2 = [5, 7, 8, 11]
+
 Dataset = recordclass(
     "Dataset", (f"wserv{n}" for n in wserv_ids), defaults=(None,) * len(wserv_ids)
 )
 
-
-v1 = Dataset()
-
-v1_grouped = Dataset()
-
-
-data_root = (
-    "/Users/tsrice/Documents/Variability_Project_2020/wuvars/Data/reduction_artifacts/"
+Dataset_v2 = recordclass(
+    "Dataset", (f"wserv{n}" for n in wserv_ids_v2), defaults=(None,) * len(wserv_ids_v2)
 )
 
-photometry_data = []
 
-for i, wserv in enumerate(wserv_ids):
+def load_v1():
+
+    v1 = Dataset()
+
+    data_root = "/Users/tsrice/Documents/Variability_Project_2020/wuvars/Data/reduction_artifacts/"
+
+    for i, wserv in enumerate(wserv_ids):
+        data_path = os.path.join(
+            data_root,
+            f"wserv{str(wserv)}",
+            f"WSERV{str(wserv)}_graded_clipped0.95_scrubbed0.1_dusted0.5_new_error_corrected.fits",
+        )
+
+        print(f"Loading WSERV{wserv} photometry data... ", end="", flush=True)
+        startTime = datetime.now()
+
+        dat = Table.read(data_path)
+        v1[i] = dat
+
+        print(
+            f"DONE (elapsed time: {(datetime.now() - startTime).total_seconds():.2f}s)"
+        )
+
+    return v1
+
+
+def load_v1_grouped(v1=None):
+
+    if v1 is None:
+        v1 = load_v1()
+
+    v1_grouped = Dataset()
+
+    for i, wserv in enumerate(wserv_ids):
+
+        print(f"Grouping WSERV{wserv} photometry data... ", end="", flush=True)
+        startTime = datetime.now()
+
+        dat = v1[i]
+        df = dat.to_pandas()
+        data_nuller(df)
+        dat_again = Table.from_pandas(df)
+        dat_by_source = dat_again.group_by("SOURCEID")
+        v1_grouped[i] = dat_by_source
+
+        print(
+            f"DONE (elapsed time: {(datetime.now() - startTime).total_seconds():.2f}s)"
+        )
+
+    return v1_grouped
+
+
+# load and group for each
+
+
+def load_wserv_v2(wserv):
+    """
+    Loads a photometry data given a WSERV id.
+
+    """
+    data_root = "/Users/tsrice/Documents/Variability_Project_2020/wuvars/Data/reduction_artifacts/"
     data_path = os.path.join(
         data_root,
         f"wserv{str(wserv)}",
@@ -44,13 +99,45 @@ for i, wserv in enumerate(wserv_ids):
     startTime = datetime.now()
 
     dat = Table.read(data_path)
-    v1[i] = dat
-    print("Grouping... ", end="", flush=True)
+    print(f"DONE (elapsed time: {(datetime.now() - startTime).total_seconds():.2f}s)")
+    return dat
+
+
+def group_wserv_v2(dat):
 
     df = dat.to_pandas()
     data_nuller(df)
     dat_again = Table.from_pandas(df)
     dat_by_source = dat_again.group_by("SOURCEID")
-    v1_grouped[i] = dat_by_source
 
-    print(f"DONE (elapsed time: {(datetime.now() - startTime).total_seconds():.2f}s)")
+    return dat_by_source
+
+
+def load_v2():
+    v2 = Dataset_v2()
+    for i, wserv in enumerate(wserv_ids_v2):
+        v2[i] = load_wserv_v2(wserv)
+
+    return v2
+
+
+def load_v2_grouped(v2=None):
+
+    if v2 is None:
+        v2 = load_v2()
+
+    v2_grouped = Dataset_v2()
+
+    for i, wserv in enumerate(wserv_ids_v2):
+
+        print(f"Grouping WSERV{wserv} photometry data... ", end="", flush=True)
+        startTime = datetime.now()
+
+        dat = v2[i]
+        v2_grouped[i] = group_wserv_v2(dat)
+
+        print(
+            f"DONE (elapsed time: {(datetime.now() - startTime).total_seconds():.2f}s)"
+        )
+
+    return v2_grouped
