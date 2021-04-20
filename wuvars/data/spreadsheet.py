5cd1df1b64234ba4736684dca8fd9177fcd5991c
Tom Rice <t.rice90@gmail.com>
2021-04-20 13:55:28 -0400

    updating the spreadsheet loading fns to match the photometry loading fns

--- wuvars/data/spreadsheet.py
+++ wuvars/data/spreadsheet.py
@@ -12,11 +12,15 @@ import pandas as pd
 from recordclass import recordclass
 
 wserv_ids = [1, 5, 7, 8, 11]
+wserv_ids_v2 = [5, 7, 8, 11]
 
 Dataset = recordclass(
     "Dataset", (f"wserv{n}" for n in wserv_ids), defaults=(None,) * len(wserv_ids)
 )
 
+Dataset_v2 = recordclass(
+    "Dataset", (f"wserv{n}" for n in wserv_ids_v2), defaults=(None,) * len(wserv_ids_v2)
+)
 
 def load_v1():
 
@@ -40,3 +44,30 @@ def load_v1():
         v1[i] = ds
 
     return v1
+
+
+def load_wserv_v2(wserv):
+    """
+    Loads a photometry data given a WSERV id.
+
+    """
+    spreadsheet_root = "/Users/tsrice/Documents/Variability_Project_2020/wuvars/Data/analysis_artifacts"
+    spreadsheet_path = os.path.join(
+        spreadsheet_root,
+        f"wserv{str(wserv)}",
+        f"WSERV{str(wserv)}_graded_clipped0.95_scrubbed0.1_dusted0.5_new_error_corrected_summary_spreadsheet.h5",
+    )
+
+    ds = pd.read_hdf(spreadsheet_path, key="table")
+    # adjust post-facto for the missing 2/3 weight.
+    ds.loc[:, ("variability", "Stetson_JHK")] *= 1 / 1.5
+
+    return ds
+
+
+def load_v2():
+    v2 = Dataset_v2()
+    for i, wserv in enumerate(wserv_ids_v2):
+        v2[i] = load_wserv_v2(wserv)
+
+    return v2
