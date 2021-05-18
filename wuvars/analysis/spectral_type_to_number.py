c61a2d42d59c1736c2d7fe02208f3305b5ce1922
Tom Rice <t.rice90@gmail.com>
2021-05-18 14:27:47 -0700

    update spectral_type_to_number to handle ALL known spectral types, at least in SpT-> num

--- wuvars/analysis/spectral_type_to_number.py
+++ wuvars/analysis/spectral_type_to_number.py
@@ -20,7 +20,18 @@ def get_num_from_SpT(SpT):
 
     letter_part = SpT[0]
 
-    letter_values = {"M": 0, "L": 10}
+    letter_values = {
+        "M": 0,
+        "L": 10,
+        "T": 20,
+        "Y": 30,
+        "K": -10,
+        "G": -20,
+        "F": -30,
+        "A": -40,
+        "B": -50,
+        "O": -60,
+    } # newly extended to cover all the spectral types I think I might ever encounter
 
     number_part = float(SpT[1:])
 
@@ -56,6 +67,8 @@ if __name__ == "__main__":
     assert get_num_from_SpT("M0") == 0
     assert get_num_from_SpT("M5.5") == 5.5
     assert get_num_from_SpT("L1") == 11
+    assert get_num_from_SpT("K9") == -1
+    assert get_num_from_SpT("G2") == -18
 
     assert get_SpT_from_num(0) == "M0"
     assert get_SpT_from_num(5.5) == "M5.5"
