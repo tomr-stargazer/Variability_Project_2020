cf7a978a2d81e3b039c14742f16923ab5cea70f8
Tom Rice <t.rice90@gmail.com>
2021-06-30 18:07:11 -0400

    generalized get_SpT_from_num to all spectral types that the inverse can handle. Passes new asserts.

--- wuvars/analysis/spectral_type_to_number.py
+++ wuvars/analysis/spectral_type_to_number.py
@@ -7,6 +7,8 @@ It's really only intended for M and L types.
 
 """
 
+import numpy as np
+
 
 def get_num_from_SpT(SpT):
     """
@@ -31,7 +33,7 @@ def get_num_from_SpT(SpT):
         "A": -40,
         "B": -50,
         "O": -60,
-    } # newly extended to cover all the spectral types I think I might ever encounter
+    }  # newly extended to cover all the spectral types I think I might ever encounter
 
     number_part = float(SpT[1:])
 
@@ -50,16 +52,28 @@ def get_SpT_from_num(num):
 
     """
 
-    if num < 10:
-        if isinstance(num, int):
-            return f"M{num}"
-        elif isinstance(num, float):
-            return f"M{num:.1f}"
-    elif num >= 10:
-        if isinstance(num, int):
-            return f"L{num-10}"
-        elif isinstance(num, float):
-            return f"L{num-10:.1f}"
+    letter_values = {
+        "M": 0,
+        "L": 10,
+        "T": 20,
+        "Y": 30,
+        "K": -10,
+        "G": -20,
+        "F": -30,
+        "A": -40,
+        "B": -50,
+        "O": -60,
+    }  # newly extended to cover all the spectral types I think I might ever encounter
+
+    letter_values_inv = {v: k for k, v in letter_values.items()}
+
+    ones_place = num % 10
+    tens_place = num - ones_place
+
+    if isinstance(num, int):
+        return f"{letter_values_inv[tens_place]}{ones_place}"
+    elif isinstance(num, float):
+        return f"{letter_values_inv[tens_place]}{ones_place:.1f}"
 
 
 if __name__ == "__main__":
@@ -73,3 +87,5 @@ if __name__ == "__main__":
     assert get_SpT_from_num(0) == "M0"
     assert get_SpT_from_num(5.5) == "M5.5"
     assert get_SpT_from_num(11) == "L1"
+    assert get_SpT_from_num(-1) == "K9"
+    assert get_SpT_from_num(-18) == "G2"
