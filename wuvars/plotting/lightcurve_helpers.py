be2f67144555ed53c13321ae00ce07edc985c908
Tom Rice <t.rice90@gmail.com>
2021-06-30 15:02:57 -0400

    copied orion_cmap from wuvars_orion

--- wuvars/plotting/lightcurve_helpers.py
+++ wuvars/plotting/lightcurve_helpers.py
@@ -4,9 +4,13 @@ Some auxiliary functions to help the lightcurves.
 At the moment I am mostly thinking of a way to produce `xlims`
 for use in `brokenaxes` axes, given arrays of date values.
 
+I've also copied in the `orion_cmap` that I used for the 2015 paper. 
+It's really useful and well-tuned to its use case.
+
 """
 
 import numpy as np
+from matplotlib.colors import LinearSegmentedColormap
 
 
 def produce_xlims(dates, breaks, pad=10):
@@ -31,6 +35,25 @@ def produce_xlims(dates, breaks, pad=10):
     xlim_list.append(np.ceil(np.max(dates)) + pad)
 
     # group xlim_list into pairs.
-    xlims = [(xlim_list[i], xlim_list[i+1]) for i in range(0, len(xlim_list), 2)]
+    xlims = [(xlim_list[i], xlim_list[i + 1]) for i in range(0, len(xlim_list), 2)]
 
     return xlims
+
+
+# fmt: off
+orion_color_dic = {'red': ((0., 1, 0),
+                           (0.4, 1, 1),
+                           (0.9, 1, 1),
+                           (1, 0.5, 0.5)),
+                   'green': ((0., 1, 0),
+                             (0.136, 1, 1),
+                             (0.8, 1, 1),
+                             (0.9, 0, 0),
+                             (1, 0, 0)),
+                   'blue': ((0., 1, 1),
+                            (0.2, 0.5, 0.5),
+                            (0.7, 0, 0),
+                            (1, 0, 0))}
+# fmt: on
+
+orion_cmap = LinearSegmentedColormap("orion_cmap", orion_color_dic)
