e10f77d99a79b16f2a530f6cb0ea031cf232695c
Tom Rice <t.rice90@gmail.com>
2021-08-16 22:36:53 -0700

    now this works for the onc! let's try it with NGC and IC.

--- wuvars/analysis/bd_matching_v2.py
+++ wuvars/analysis/bd_matching_v2.py
@@ -36,6 +36,12 @@ Our goal:
 That's the paper. Periods, interesting lightcurves, K band amplitude stats, and 
 color variability.
 
+* 'confident member' means:
+(a) listed in Tables 1 or 2 of Luhman+16 (for Perseus), or
+(b) Bayes Factor > 99 i.e., ["log(BF)"] > np.log10(99) (for Orion)
+
+** matching radius is 0.37'', chosen by histogram analysis (by eye).
+
 """
 
 
@@ -47,7 +53,7 @@ from astropy.coordinates import SkyCoord, Angle
 from astropy import units as u
 import pandas as pd
 
-from wuvars.data import spreadsheet, photometry
+from wuvars.data import spreadsheet, photometry, quality_classes
 from wuvars.analysis.luhman16_coord_handler import coords_from_Luhman_table
 from wuvars.analysis.spectral_type_to_number import get_num_from_SpT, get_SpT_from_num
 from wuvars.analysis.spectral_type_to_temperature import (
@@ -55,18 +61,32 @@ from wuvars.analysis.spectral_type_to_temperature import (
     get_SpT_from_Teff,
 )
 
+
 class MatchStruct:
     pass
 
 
 lc_dir = "/Users/tsrice/Documents/Variability_Project_2020/wuvars/analysis/prototypes/BD_lcs_v3"
-inspect_onc = pd.read_csv(os.path.join(lc_dir, "inspection_onc.csv"), skipinitialspace=True)
-inspect_ngc = pd.read_csv(os.path.join(lc_dir, "inspection_ngc.csv"), skipinitialspace=True)
-inspect_ic = pd.read_csv(os.path.join(lc_dir, "inspection_ic.csv"), skipinitialspace=True)
+inspect_onc = pd.read_csv(
+    os.path.join(lc_dir, "inspection_onc.csv"), skipinitialspace=True
+)
+inspect_ngc = pd.read_csv(
+    os.path.join(lc_dir, "inspection_ngc.csv"), skipinitialspace=True
+)
+inspect_ic = pd.read_csv(
+    os.path.join(lc_dir, "inspection_ic.csv"), skipinitialspace=True
+)
 
+rejected_sources_onc = inspect_onc["SOURCEID"][inspect_onc["exclude?"] == "yes"]
+rejected_sources_ngc = inspect_ngc["SOURCEID"][inspect_ngc["exclude?"] == "yes"]
+rejected_sources_ic = inspect_ic["SOURCEID"][inspect_ic["exclude?"] == "yes"]
 
-if __name__ == "__main__":
-    # def match_onc():
+approved_sources_onc = inspect_onc["SOURCEID"][inspect_onc["exclude?"] != "yes"]
+approved_sources_ngc = inspect_ngc["SOURCEID"][inspect_ngc["exclude?"] != "yes"]
+approved_sources_ic = inspect_ic["SOURCEID"][inspect_ic["exclude?"] != "yes"]
+
+# if __name__ == "__main__":
+def match_onc():
 
     onc_match = MatchStruct()
 
@@ -86,7 +106,7 @@ if __name__ == "__main__":
 
     robb_joined = astropy.table.join(robb_table2, robb_table4)
     onc_spt = np.array([get_SpT_from_Teff(x) for x in robb_joined["Teff"]])
-    robb_joined.add_column("SpT", onc_spt)
+    robb_joined["SpT"] = onc_spt
 
     robb_coordinates = SkyCoord(ra=robb_joined["RAdeg"], dec=robb_joined["DEdeg"])
 
@@ -117,6 +137,8 @@ if __name__ == "__main__":
         ra=sm["RA"].values * u.rad, dec=sm["DEC"].values * u.rad
     )
 
+    onc_q = quality_classes.load_q(5)
+
     # =================================== #
     # === Part 3: Doing the matching. === #
     # =================================== #
@@ -127,18 +149,14 @@ if __name__ == "__main__":
     max_sep = 0.37 * u.arcsec
     sep_constraint = d2d < max_sep
 
-    # # We're going to compute
-    # # (a) all matched NGC1333 members, for quality control / comparisons, and
-    # # (b) just the brown dwarfs
-
     matches_sm = sm.iloc[idx[sep_constraint & confident_members]]
-    matches_sm.rename(columns=lambda name: "median_" + name)
+    matches_sm = matches_sm.rename(columns=lambda name: "median_" + name)
     matches_sv = sv.iloc[idx[sep_constraint & confident_members]]
-    matches_sv.rename(columns=lambda name: "var_" + name)
+    matches_sv = matches_sv.rename(columns=lambda name: "var_" + name)
     matches_ss = ss.iloc[idx[sep_constraint & confident_members]]
-    matches_ss.rename(columns=lambda name: "std_" + name)
+    matches_ss = matches_ss.rename(columns=lambda name: "std_" + name)
     matches_sr = sr.iloc[idx[sep_constraint & confident_members]]
-    matches_sr.rename(columns=lambda name: "range_" + name)
+    matches_sr = matches_sr.rename(columns=lambda name: "range_" + name)
 
     matched = robb_joined[sep_constraint & confident_members]
     joint_matches = astropy.table.hstack(
@@ -153,24 +171,23 @@ if __name__ == "__main__":
     joint_matches.add_column(matches_sm.index, index=0, name="SOURCEID")
 
     onc_match.all_matches = joint_matches
-    onc_match.lowmass = joint_matches[joint_matches['Teff'] < 3200]
-    onc_match.approved = joint_matches[joint_matches['Teff'] < 3200]
 
+    lowmass_criteria = joint_matches["Teff"] < 3200
+    onc_match.lowmass = joint_matches[lowmass_criteria]
+    onc_match.not_lowmass = joint_matches[~lowmass_criteria]
 
-    # bd_matches = sm.iloc[idx[sep_constraint & confident_bds]]
-    # bd_matched = robb_joined[sep_constraint & confident_bds]
-    # bd_joint_matches = astropy.table.hstack(
-    #     [astropy.table.Table.from_pandas(bd_matches), bd_matched]
-    # )
-    # bd_joint_matches.add_column(bd_matches.index, index=0, name="SOURCEID")
+    approved_indices_onc = np.in1d(joint_matches["SOURCEID"], approved_sources_onc)
 
-    # lowmass_matches = sm.iloc[idx[sep_constraint & confident_lowmass]]
-    # lowmass_matches_spread = spread.iloc[idx[sep_constraint & confident_lowmass]]
-    # lowmass_matched = robb_joined[sep_constraint & confident_lowmass]
-    # lowmass_joint_matches = astropy.table.hstack(
-    #     [astropy.table.Table.from_pandas(lowmass_matches), lowmass_matched]
-    # )
-    # lowmass_joint_matches.add_column(lowmass_matches.index, index=0, name="SOURCEID")
+    approved_criteria = lowmass_criteria & approved_indices_onc
+    onc_match.approved = joint_matches[approved_criteria]
+
+    statistical_criteria = approved_criteria & (
+        joint_matches["median_KAPERMAG3ERR"] < 0.05
+    ) & onc_q.q1_k[joint_matches['SOURCEID']].values
+    onc_match.statistical = joint_matches[statistical_criteria]
+
+    color_criteria = statistical_criteria & onc_q.q2[joint_matches['SOURCEID']].values
+    onc_match.color = joint_matches[color_criteria]
 
 # else:
-#     return data_struct
+    return onc_match
