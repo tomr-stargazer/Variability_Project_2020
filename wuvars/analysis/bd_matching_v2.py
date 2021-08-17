c443c572c12d6b73642b927650669745fbc2efc2
Tom Rice <t.rice90@gmail.com>
2021-08-16 20:35:15 -0700

    getting a skeleton together

---
+++ wuvars/analysis/bd_matching_v2.py
@@ -0,0 +1,176 @@
+"""
+Working on a unified bd_matching approach.
+
+Prototyping it in script form here.
+
+There are several different subsets which are defined in code below.
+Each subset is a strict subset of the class immediately above it; 
+imagine nesting Russian dolls, not a Venn diagram with complicated overlaps.
+
+Here are my english-language definitions:
+- all_matches: 
+    every source which is
+    (a) a confident member of the cluster in question*, and
+    (b) matches within some matching radius** of a UKIRT source.
+- lowmass:
+    subset of the above which has either
+    (a) Teff < 3200 (if Teff is primary), or 
+    (b) SpT >= M4.5 (if SpT is primary).
+- approved:
+    subset of the above which
+    (a) upon my visual inspection of its lightcurve, did not have data 
+        poor enough to say "there is no useful variability information here"
+- statistical:
+    subset of the above which
+    (a) has Q1_k quality (at least) and
+    (b) has median K error below 0.05 mag
+- color:
+    subset of the above which
+    (a) has Q2 quality
+
+Our goal:
+- check for periods and "interesting" lightcurves among all `approved`
+- do K-band variability amplitude stats on `statistical`
+- search for color variability stats among `color` 
+
+That's the paper. Periods, interesting lightcurves, K band amplitude stats, and 
+color variability.
+
+"""
+
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import astropy.table
+from astropy.coordinates import SkyCoord, Angle
+from astropy import units as u
+import pandas as pd
+
+from wuvars.data import spreadsheet, photometry
+from wuvars.analysis.luhman16_coord_handler import coords_from_Luhman_table
+from wuvars.analysis.spectral_type_to_number import get_num_from_SpT, get_SpT_from_num
+from wuvars.analysis.spectral_type_to_temperature import (
+    get_Teff_from_SpT,
+    get_SpT_from_Teff,
+)
+
+class MatchStruct:
+    pass
+
+
+lc_dir = "/Users/tsrice/Documents/Variability_Project_2020/wuvars/analysis/prototypes/BD_lcs_v3"
+inspect_onc = pd.read_csv(os.path.join(lc_dir, "inspection_onc.csv"), skipinitialspace=True)
+inspect_ngc = pd.read_csv(os.path.join(lc_dir, "inspection_ngc.csv"), skipinitialspace=True)
+inspect_ic = pd.read_csv(os.path.join(lc_dir, "inspection_ic.csv"), skipinitialspace=True)
+
+
+if __name__ == "__main__":
+    # def match_onc():
+
+    onc_match = MatchStruct()
+
+    # =========================================== #
+    # === Part 1: loading the input catalogs. === #
+    # =========================================== #
+
+    aux_path = "/Users/tsrice/Documents/Variability_Project_2020/wuvars/data/auxiliary_catalogs/ONC"
+
+    # Robberto et al. 2020, Table 2: WFC3-IR Photometry of ONC Sources
+    filepath_table2 = os.path.join(aux_path, "Robberto_2020_Table2_apjab911et2_mrt.txt")
+    robb_table2 = astropy.table.Table.read(filepath_table2, format="ascii")[:-1]
+
+    # Robberto et al. 2020, Table 4: Estimated Parameters of the ONC Sample
+    filepath_table4 = os.path.join(aux_path, "Robberto_2020_Table4_apjab911et4_mrt.txt")
+    robb_table4 = astropy.table.Table.read(filepath_table4, format="ascii")
+
+    robb_joined = astropy.table.join(robb_table2, robb_table4)
+    onc_spt = np.array([get_SpT_from_Teff(x) for x in robb_joined["Teff"]])
+    robb_joined.add_column("SpT", onc_spt)
+
+    robb_coordinates = SkyCoord(ra=robb_joined["RAdeg"], dec=robb_joined["DEdeg"])
+
+    # # This column contains the Teff.
+    confident_members = robb_joined["log(BF)"] > np.log10(99)
+    confident_bds = (robb_joined["Mstar"] < 0.08) & (
+        robb_joined["log(BF)"] > np.log10(99)
+    )
+
+    confident_lowmass = (robb_joined["Teff"] <= 3200) & (
+        robb_joined["log(BF)"] > np.log10(99)
+    )
+
+    # onc_spt = np.array([get_SpT_from_Teff(x) for x in lowmass_matched["Teff"]])
+    # not_lowmass_onc_spt = np.array([get_SpT_from_Teff(x) for x in matched['Teff']])
+
+    # ======================================= #
+    # === Part 2: loading our UKIRT data. === #
+    # ======================================= #
+
+    # ONC is WSERV5
+    spread = spreadsheet.load_wserv_v2(5)
+    sm = spread["median"]
+    sv = spread["variability"]
+    ss = spread["std"]
+    sr = spread["range_9010"]
+    spreadsheet_coordinates = SkyCoord(
+        ra=sm["RA"].values * u.rad, dec=sm["DEC"].values * u.rad
+    )
+
+    # =================================== #
+    # === Part 3: Doing the matching. === #
+    # =================================== #
+
+    idx, d2d, d3d = robb_coordinates.match_to_catalog_sky(spreadsheet_coordinates)
+
+    # Maximum separation of 0.37'' chosen by inspecting a histogram of closest matches.
+    max_sep = 0.37 * u.arcsec
+    sep_constraint = d2d < max_sep
+
+    # # We're going to compute
+    # # (a) all matched NGC1333 members, for quality control / comparisons, and
+    # # (b) just the brown dwarfs
+
+    matches_sm = sm.iloc[idx[sep_constraint & confident_members]]
+    matches_sm.rename(columns=lambda name: "median_" + name)
+    matches_sv = sv.iloc[idx[sep_constraint & confident_members]]
+    matches_sv.rename(columns=lambda name: "var_" + name)
+    matches_ss = ss.iloc[idx[sep_constraint & confident_members]]
+    matches_ss.rename(columns=lambda name: "std_" + name)
+    matches_sr = sr.iloc[idx[sep_constraint & confident_members]]
+    matches_sr.rename(columns=lambda name: "range_" + name)
+
+    matched = robb_joined[sep_constraint & confident_members]
+    joint_matches = astropy.table.hstack(
+        [
+            astropy.table.Table.from_pandas(matches_sm),
+            astropy.table.Table.from_pandas(matches_sv),
+            astropy.table.Table.from_pandas(matches_ss),
+            astropy.table.Table.from_pandas(matches_sr),
+            matched,
+        ]
+    )
+    joint_matches.add_column(matches_sm.index, index=0, name="SOURCEID")
+
+    onc_match.all_matches = joint_matches
+    onc_match.lowmass = joint_matches[joint_matches['Teff'] < 3200]
+    onc_match.approved = joint_matches[joint_matches['Teff'] < 3200]
+
+
+    # bd_matches = sm.iloc[idx[sep_constraint & confident_bds]]
+    # bd_matched = robb_joined[sep_constraint & confident_bds]
+    # bd_joint_matches = astropy.table.hstack(
+    #     [astropy.table.Table.from_pandas(bd_matches), bd_matched]
+    # )
+    # bd_joint_matches.add_column(bd_matches.index, index=0, name="SOURCEID")
+
+    # lowmass_matches = sm.iloc[idx[sep_constraint & confident_lowmass]]
+    # lowmass_matches_spread = spread.iloc[idx[sep_constraint & confident_lowmass]]
+    # lowmass_matched = robb_joined[sep_constraint & confident_lowmass]
+    # lowmass_joint_matches = astropy.table.hstack(
+    #     [astropy.table.Table.from_pandas(lowmass_matches), lowmass_matched]
+    # )
+    # lowmass_joint_matches.add_column(lowmass_matches.index, index=0, name="SOURCEID")
+
+# else:
+#     return data_struct
