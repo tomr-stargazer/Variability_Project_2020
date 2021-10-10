ced36d87f41bd902d9c75f5c602e4ab3b0f4a28b
Tom Rice <t.rice90@gmail.com>
2021-10-09 20:03:59 -0400

    detrending now has bones

--- wuvars/analysis/detrending.py
+++ wuvars/analysis/detrending.py
@@ -7,10 +7,12 @@ from numpy.polynomial.polynomial import polyfit, polyval
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-# from astropy.convolution import convolve, Box1DKernel
-# rv_smooth = convolve(rv, Box1DKernel(100))
-# rv_hpf = rv - rv_smooth
+from astropy.timeseries import LombScargle
+from wuvars.analysis.periods import f_max, f_min, N_eval
+from wuvars.plotting.lightcurve import (
+    simple_lc_scatter_brokenaxes,
+    simple_phased_lc_scatter_gridspec,
+)
 
 
 def poly_detrend(
@@ -62,3 +64,97 @@ def poly_detrend(
         fit_dat[f"{band}APERMAG3"] = fit_mag
 
     return detrended_dat, fit_dat
+
+
+def visualize_detrending(
+    sid,
+    dat,
+    date_offset,
+    start=0,
+    end=np.inf,
+    poly_order=1,
+    nterms=1,
+    cmap="jet_r",
+    simple_breaks=None,
+    plot_phase=False,
+    force_period=False
+):
+
+    
+    if len(cmap) == 2:
+        cmap_1 = cmap[0]
+        cmap_2 = cmap[1]
+    else:
+        cmap_1 = cmap
+        cmap_2 = cmap
+
+    fig_1 = simple_lc_scatter_brokenaxes(
+        dat, sid, date_offset=date_offset, cmap=cmap_1, breaks=simple_breaks
+    )
+
+    dat_sid = dat.groups[dat.groups.keys["SOURCEID"] == sid]
+    detrended, fit = poly_detrend(
+        dat_sid, date_offset, start, end, poly_order=poly_order
+    )
+
+    fig_2 = simple_lc_scatter_brokenaxes(
+        detrended.group_by("SOURCEID"), sid, cmap=cmap_2, breaks=[]
+    )
+    fig_3 = simple_lc_scatter_brokenaxes(
+        fit.group_by("SOURCEID"), sid, cmap=cmap_2, breaks=[]
+    )
+
+    star_dat = detrended.group_by("SOURCEID")
+
+    freq = np.linspace(f_min, f_max, N_eval)
+
+    periods = []
+
+    for band in ["J", "H", "K"]:
+        # band = 'K'
+        mask = star_dat[f"{band}APERMAG3"].mask
+        times = star_dat["MEANMJDOBS"][~mask]
+        mags = star_dat[f"{band}APERMAG3"][~mask]
+        errs = star_dat[f"{band}APERMAG3ERR"][~mask]
+
+        ls = LombScargle(times, mags, dy=errs, nterms=nterms)
+        power = ls.power(freq, assume_regular_frequency=True).value
+
+        min_freq = 1 / 100
+        power[freq < min_freq] = 0
+        power[np.abs(freq - 1) < 0.01] = 0
+
+        # powermax = np.nanmax(power)
+        fmax = freq[np.nanargmax(power)]
+        #         fap = ls.false_alarm_probability(np.nanmax(power)).value
+
+        plt.figure()
+        plt.plot(1 / freq, power, "k", rasterized=True)
+        plt.semilogx()
+        plt.xlim(0.1, 10)
+
+        print(f"{band} period: {1 / fmax:.4f}")
+        try:
+            amp = np.sqrt(np.sum(ls.model_parameters(fmax)[1:3] ** 2))
+            print(f"{band} amp: {amp:.3f}")
+        except:
+            pass
+
+        periods.append(1 / fmax)
+
+    if plot_phase:
+
+        if force_period:
+            period = force_period
+        else:
+
+            if np.abs(periods[0] - periods[2])/periods[2] < 0.01:
+                period = np.mean(periods)
+                print("periods agree: ", periods)
+            else:
+                period = periods[2]
+                print("periods disagree: ", periods, "; using K")
+
+        fig_4 = simple_phased_lc_scatter_gridspec(
+            star_dat, sid, period, cmap=cmap_2
+        )
