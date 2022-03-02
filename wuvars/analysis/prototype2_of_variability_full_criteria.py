6f6e643e1b592e82137d26251975abdbba655adf
Tom Rice <t.rice90@gmail.com>
2022-03-01 19:50:41 -0500

    a pair of prototype scripts; will rename one eventually.

---
+++ wuvars/analysis/prototype2_of_variability_full_criteria.py
@@ -0,0 +1,197 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from wuvars.data import spreadsheet, photometry, quality_classes
+import wuvars.analysis.variability_selection as sv
+from wuvars.analysis.bd_matching_v2 import match_onc, match_ngc, match_ic
+from wuvars.analysis.variability_selection_curved import (
+    curve_Stetson,
+    sv_jh,
+    sv_hk,
+    sv_jk,
+    sv_jhk,
+)
+
+# general setup
+
+spread = spreadsheet.load_v2()
+stetson_directory = (
+    "/Users/tsrice/Documents/Variability_Project_2020/wuvars/analysis/stetson_v_mag"
+)
+period_directory = (
+    "/Users/tsrice/Documents/Variability_Project_2020/wuvars/analysis/prototypes"
+)
+subj_directory = period_directory
+subj_sheets = pd.read_excel(
+    os.path.join(subj_directory, "Q0_nonperiodic_followup.xlsx"), sheet_name=None
+)
+
+# in practice we don't use wserv 1 or 11, but I wrote this out and so I'll keep it
+wserv_ids = [1, 5, 7, 8, 11]
+n_obs_list = [130, 120, 171, 85, 110]
+
+n_min_list = [60, 35, 80, 55, 65]
+n_max_list = [100, 90, 160, 80, 100]
+
+n_min_dict = {wserv: n_min for (wserv, n_min) in zip(wserv_ids, n_min_list)}
+n_max_dict = {wserv: n_max for (wserv, n_max) in zip(wserv_ids, n_max_list)}
+
+onc_match = match_onc()
+ngc_match = match_ngc()
+ic_match = match_ic()
+
+reference_dict = {5: onc_match, 7: ngc_match, 8: ic_match}
+short_names = {5: "ONC", 7: "NGC", 8: "IC"}
+
+
+def select_targets(wserv, attr="approved"):
+
+    sourceids = getattr(reference_dict[wserv], attr)["SOURCEID"]
+
+    ds = spread[f"wserv{wserv}"]
+    ref = np.in1d(ds.index, sourceids)
+
+    return ref
+
+
+def select_stetson_variables(wserv):
+
+    n_min = n_min_dict[wserv]
+    n_max = n_max_dict[wserv]
+
+    stetson_result_997 = np.load(
+        os.path.join(stetson_directory, f"WSERV{wserv}_result_grid_997.npy")
+    )
+
+    stetson_result_95 = np.load(
+        os.path.join(stetson_directory, f"WSERV{wserv}_result_grid_95.npy")
+    )
+
+    ds = spread[f"wserv{wserv}"]
+    q0 = sv.sq0(ds, n_min, n_max)
+    #         q1 = sq1(ds, n_min, n_max)
+
+    q1_j = sv.sq1_j(ds, n_min, n_max)
+    q1_h = sv.sq1_h(ds, n_min, n_max)
+    q1_k = sv.sq1_k(ds, n_min, n_max)
+
+    q2 = sv.sq2(ds, n_min, n_max)
+
+    suffix_997 = "_m_997"
+
+    curve_Stetson(ds, stetson_result_997[0], stetson_result_997[1], "_m_997")
+    curve_Stetson(ds, stetson_result_95[0], stetson_result_95[1], "_m_95")
+    for bands in ["JH", "JK", "HK"]:
+        curve_Stetson(
+            ds,
+            stetson_result_997[0],
+            stetson_result_997[1],
+            "_m_997",
+            input_column=f"Stetson_{bands}",
+        )
+
+    v_jhk = sv_jhk(ds, Stetson_cutoff=0, suffix=suffix_997)
+    #         v2 = sq2_variables()
+    v_jh = sv_jh(ds, Stetson_cutoff=0, suffix=suffix_997)
+    v_jk = sv_jk(ds, Stetson_cutoff=0, suffix=suffix_997)
+    v_hk = sv_hk(ds, Stetson_cutoff=0, suffix=suffix_997)
+
+    v2 = q2 & (v_jhk | v_jk | v_hk | v_jh)
+
+    v1_jh = q1_j & q1_h & v_jh
+    v1_jk = q1_j & q1_k & v_jk
+    v1_hk = q1_h & q1_k & v_hk
+
+    v1 = (
+        (v1_jh & q1_j & q1_h)
+        | (v1_hk & q1_k & q1_h)
+        | (v1_jk & q1_j & q1_k)
+        | (q2 & (v_jhk | v_jk | v_hk | v_jh))
+    )
+
+    return v2, v1
+
+
+def select_periodic_variables(wserv):
+
+    ds = spread[f"wserv{wserv}"]
+
+    flags = ["Y", "Yw", "N", "YfY", "?fY", "YfYw", "?fYw", "YfN", "?fN"]
+    periodic_flags = [flag for flag in flags if flag[-1] in ("Y", "w")]
+
+    period_sheet = pd.read_excel(
+        os.path.join(
+            period_directory,
+            f"{short_names[wserv]}_source_properties_periods_inspected.xlsx",
+        )
+    )
+    periodics = period_sheet[np.in1d(period_sheet["Periodic?"], periodic_flags)]
+
+    v_per = np.in1d(ds.index, periodics.values)
+
+    return v_per
+
+
+def select_subjective_variables(wserv):
+
+    ds = spread[f"wserv{wserv}"]
+
+    q0_variables = subj_sheets[short_names[wserv]]["SOURCEID"][
+        subj_sheets[short_names[wserv]]["VARIABLE"] == 1
+    ]
+    # ngc_v0 = np.in1d(ngc_match.approved['SOURCEID'], q0_variables)
+    v_subj = np.in1d(ds.index, q0_variables)
+
+    return v_subj
+
+
+def select_variables(wserv):
+
+    ref = select_targets(wserv)
+
+    v2, v1 = select_stetson_variables(wserv)
+    v_per = select_periodic_variables(wserv)
+    v_subjective = select_subjective_variables(wserv)
+
+    vv = ref & (v2 | v1 | v_per | v_subjective)
+
+    n_variables = np.sum(ref & (v2 | v1 | v_per | v_subjective))
+    n_total = np.sum(ref)
+
+    print(n_variables, n_total)
+
+    return vv
+
+
+if __name__ == "__main__":
+
+    for wserv in [5, 7, 8]:
+        print(f"WSERV{wserv}:")
+
+        ref = select_targets(wserv)
+        stat = select_targets(wserv, attr="statistical")
+        v2, v1 = select_stetson_variables(wserv)
+        v_per = select_periodic_variables(wserv)
+        v_subj = select_subjective_variables(wserv)
+
+        print(f"Ref: {np.sum(ref)} (Stat: {np.sum(stat)})")
+        print(f"v2: {np.sum(ref & v2)} ({np.sum(stat & v2)})")
+        print(f"v1: {np.sum(ref & v1)} ({np.sum(stat & v1)})")
+        print(f"v_per: {np.sum(ref & v_per)} ({np.sum(stat & v_per)})")
+        print(f"v_subj: {np.sum(ref & v_subj)} ({np.sum(stat & v_subj)})")
+
+        vv = select_variables(wserv)
+
+        print(f"v_total: {np.sum(ref & vv)} ({np.sum(stat & vv)})")
+        rate = np.sum(stat & vv) / np.sum(stat)
+        per_rate = np.sum(stat & v_per) / np.sum(stat)
+        print(
+            f"Statistical variability rate: {np.sum(stat & vv)}/{np.sum(stat)} = {rate:.2f}"
+        )
+        print(
+            f"Statistical periodicity rate: {np.sum(stat & v_per)}/{np.sum(stat)} = {per_rate:.2f}"
+        )
+
+        print("")
---
+++ wuvars/analysis/prototype_of_variability_full_criteria.py
@@ -0,0 +1,168 @@
+# variable selection - a script.
+
+# imagine we are loading things up just for NGC 1333.
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from wuvars.data import spreadsheet, photometry, quality_classes
+import wuvars.analysis.variability_selection as sv
+from wuvars.analysis.bd_matching_v2 import match_ngc
+from wuvars.analysis.variability_selection_curved import (
+    curve_Stetson,
+    sv_jh,
+    sv_hk,
+    sv_jk,
+    sv_jhk,
+)
+
+spread = spreadsheet.load_v2()
+
+ngc_match = match_ngc()
+
+load_dir = "/Users/tsrice/Documents/Variability_Project_2020/wuvars/analysis/stetson_v_mag"
+
+StetsonJHK_dict = {}
+StetsonJHK_dict_95 = {}
+StetsonJHK_dict_50 = {}
+wserv = 7
+r997 = np.load(
+    os.path.join(load_dir, f"WSERV{wserv}_result_grid_997.npy")
+)
+StetsonJHK_dict[wserv] = r997
+r95 = np.load(
+    os.path.join(load_dir, f"WSERV{wserv}_result_grid_95.npy")
+)
+StetsonJHK_dict_95[wserv] = r95    
+r50 = np.load(
+    os.path.join(load_dir, f"WSERV{wserv}_result_grid_50.npy")
+)
+StetsonJHK_dict_50[wserv] = r50     
+flags = ['Y', 'Yw', 'N', 'YfY', '?fY', 'YfYw', '?fYw', "YfN", "?fN"]
+periodic_flags = [flag for flag in flags if flag[-1] in ('Y', 'w')]
+spreadsheet_dir = "/Users/tsrice/Documents/Variability_Project_2020/wuvars/analysis/prototypes"
+
+period_sheet_ngc = pd.read_excel(
+    os.path.join(spreadsheet_dir, "NGC_source_properties_periods_inspected.xlsx")
+)
+periodic_ngc = period_sheet_ngc[np.in1d(period_sheet_ngc['Periodic?'], periodic_flags)]
+
+periodic = {}
+periodic[7] = periodic_ngc
+
+lc_dir = "/Users/tsrice/Documents/Variability_Project_2020/wuvars/analysis/prototypes/BD_lcs_v3"
+
+inspect_ngc = pd.read_csv(os.path.join(lc_dir, "inspection_ngc.csv"), skipinitialspace=True)
+
+approved_sources_ngc = inspect_ngc['SOURCEID'][inspect_ngc['exclude?'] != 'yes']
+approved = {}
+approved[7] = approved_sources_ngc
+
+# these are some parameters that are considered "input"
+wserv = 7
+n_min = 80
+n_max = 160
+
+
+ds = spread[f"wserv{wserv}"]
+
+q0 = sv.sq0(ds, n_min, n_max)
+#         q1 = sq1(ds, n_min, n_max)
+
+q1_j = sv.sq1_j(ds, n_min, n_max)
+q1_h = sv.sq1_h(ds, n_min, n_max)
+q1_k = sv.sq1_k(ds, n_min, n_max)
+
+q2 = sv.sq2(ds, n_min, n_max)
+
+#         v0 = sq0_variables(ds, n_min, n_max, Stetson_cutoff=S)
+#         v1 = sq1_variables(ds, n_min, n_max, Stetson_cutoff=S)
+#         v2 = sq2_variables(ds, n_min, n_max, Stetson_cutoff=S)
+suffix_997 = "_m_997"
+suffix_95 = "_m_95"
+curve_Stetson(ds, StetsonJHK_dict[wserv][0], StetsonJHK_dict[wserv][1], "_m_997")
+curve_Stetson(ds, StetsonJHK_dict_95[wserv][0], StetsonJHK_dict_95[wserv][1], "_m_95")
+for bands in ["JH", "JK", "HK"]:
+    curve_Stetson(
+        ds,
+        StetsonJHK_dict[wserv][0],
+        StetsonJHK_dict[wserv][1],
+        "_m_997",
+        input_column=f"Stetson_{bands}",
+    )
+
+v_jhk = sv_jhk(ds, Stetson_cutoff=0, suffix=suffix_997)
+#         v2 = sq2_variables()
+v_jh = sv_jh(ds, Stetson_cutoff=0, suffix=suffix_997)
+v_jk = sv_jk(ds, Stetson_cutoff=0, suffix=suffix_997)
+v_hk = sv_hk(ds, Stetson_cutoff=0, suffix=suffix_997)
+
+v2 = q2 & (v_jhk | v_jk | v_hk | v_jh)
+
+v1_jh = q1_j & q1_h & v_jh
+v1_jk = q1_j & q1_k & v_jk
+v1_hk = q1_h & q1_k & v_hk
+
+v1 = (
+    (v1_jh & q1_j & q1_h)
+    | (v1_hk & q1_k & q1_h)
+    | (v1_jk & q1_j & q1_k)
+    | (q2 & (v_jhk | v_jk | v_hk | v_jh))
+)
+
+v_ = v1 | v2
+
+v0 = (v_jhk | v1_jh | v1_jk | v1_hk) & ~v1 & ~v2
+
+v_jhk_cand = sv_jhk(ds, Stetson_cutoff=0, suffix=suffix_95)
+
+v_cand = q2 & v_jhk_cand & ~v_
+
+bd = np.in1d(ds.index, approved[wserv].values)
+per_bd = np.in1d(ds.index, periodic[wserv].values)
+
+print(f"WSERV{wserv}:")
+print(f"  VLMS stars in our data: {np.sum(bd)}")
+print(f"  VLMS Q=2 stars: {np.sum(bd & q2)}")
+print(f"  VLMS Q=2 variables (ignoring periodicity): {np.sum(bd & q2 & v2)}")
+print(f"  VLMS Q=2 'almost' variables: {np.sum(bd & q2 & v_jhk_cand & ~v2)}")
+print(f"  VLMS Q=2 periodic: {np.sum(bd & q2 & per_bd)}")
+print(f"  VLMS Q=2 variables (incl. periodics): {np.sum(bd & q2 & (v2 | per_bd))}")
+print("")
+print(f"BDs:                {np.sum(bd)}")
+print(f"BDs (Q=2):          {np.sum(bd & q2)}")
+print(f"Periodic BDs:       {np.sum(bd & per_bd)}")
+print(f"Periodic BDs (Q=2): {np.sum(bd & per_bd & q2)}")
+
+print(f"Variable BDs (Q=2): {np.sum(bd & v2)}")
+print(f"  '' - periodics  : {np.sum(bd & v2 & ~per_bd)}")
+print(f"Variable BDs (Q=1): {np.sum(bd & v1)}")
+print(f"  '' - v2:          {np.sum(bd & v1 & ~v2)}")
+
+print(f"V0 BDs (Q=0):       {np.sum(bd & v0)}")
+print(f"Variable BDs (Q=1+2+per): {np.sum(bd & (v1 | v2 | per_bd))}")
+print("\n\n")
+
+
+print("NGC 1333:")
+for attr, value in ngc_match.__dict__.items():
+    if attr != 'not_lowmass':
+        print(f"{attr:13s}", len(value))
+
+sheets = pd.read_excel("Q0_nonperiodic_followup.xlsx", sheet_name=None)
+
+q0_variables = sheets['NGC']['SOURCEID'][sheets['NGC']['VARIABLE'] == 1]
+
+ngc_v0 = np.in1d(ngc_match.approved['SOURCEID'], q0_variables)
+print(len(ngc_match.approved[ngc_v0]))        
+v_per = np.in1d(ds.index, periodic[wserv].values)
+
+v0_conf = np.in1d(ds.index, q0_variables)
+
+ref = np.in1d(ds.index, ngc_match.approved['SOURCEID'])
+print(np.sum(ref & q2), np.sum(ref & v2), np.sum(ref & v1), np.sum(ref & v_per), np.sum(ref & q2 & v_per), np.sum(ref & v0_conf))
+
+
+print(np.sum(ref & (v2 | v1 | v_per | v0_conf)), np.sum(ref))
