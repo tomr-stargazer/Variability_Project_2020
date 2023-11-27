9fd2a7c88dd2572f1fcf60873df30014d5e33d4d
Tom Rice <t.rice90@gmail.com>
2023-11-27 02:51:28 -0500

    just catching the commit up to where it was when I left it over a year ago

--- wuvars/analysis/prototype2_of_variability_full_criteria.py
+++ wuvars/analysis/prototype2_of_variability_full_criteria.py
@@ -2,6 +2,9 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import astropy.table
+import astropy.coordinates
+import astropy.units as u
 
 from wuvars.data import spreadsheet, photometry, quality_classes
 import wuvars.analysis.variability_selection as sv
@@ -38,9 +41,99 @@ n_max_list = [100, 90, 160, 80, 100]
 n_min_dict = {wserv: n_min for (wserv, n_min) in zip(wserv_ids, n_min_list)}
 n_max_dict = {wserv: n_max for (wserv, n_max) in zip(wserv_ids, n_max_list)}
 
+
+def augment_oncmatch_with_disk_column(onc_match, attr="approved"):
+    """Alters it in-place."""
+
+    onc_table = getattr(onc_match, attr)
+
+    all_truncated_fname = "/Users/tsrice/Desktop/Bo_Tom/aux_catalogs/spitzer_orion_survey_082112_all_truncated.fits"
+    megeath_all_truncated = astropy.table.Table.read(
+        all_truncated_fname
+    )  # 3654 members
+    megeath_all_truncated.add_index("IDL_index")
+
+    megeath_disks_fname = "/Users/tsrice/Documents/Variability_Project_2020/wuvars/analysis/prototypes/Megeath2012_table1.txt"
+    megeath_disks = astropy.table.Table.read(
+        megeath_disks_fname, format="cds"
+    )  # 3479 members
+    megeath_disks.add_index("Num")
+
+    ra = astropy.coordinates.Angle(
+        (
+            megeath_disks["RAh"].data,
+            megeath_disks["RAm"].data,
+            megeath_disks["RAs"].data,
+        ),
+        unit="hourangle",
+    )
+    dec = astropy.coordinates.Angle(
+        (
+            np.where(megeath_disks["DE-"] == "+", 1, -1) * megeath_disks["DEd"],
+            megeath_disks["DEm"],
+            megeath_disks["DEs"],
+        ),
+        unit="deg",
+    )
+
+    megeath_disk_coords = astropy.coordinates.SkyCoord(ra=ra, dec=dec)
+    megeath_all_coords = astropy.coordinates.SkyCoord(
+        ra=(megeath_all_truncated["RA"] * u.deg),
+        dec=(megeath_all_truncated["Dec"] * u.deg),
+    )
+    onc_approved_coords = astropy.coordinates.SkyCoord(
+        ra=onc_table["RAdeg"], dec=onc_table["DEdeg"]
+    )
+
+    idx, d2d, d3d = onc_approved_coords.match_to_catalog_sky(megeath_disk_coords)
+    idx_a, d2d_a, d3d_a = onc_approved_coords.match_to_catalog_sky(megeath_all_coords)
+
+    max_sep = 0.5 * u.arcsec
+    sep_constraint = d2d < max_sep
+    sep_constraint_a = d2d_a < max_sep
+
+    # MD: Megeath Disks
+    matched_md = onc_table[sep_constraint]
+    matches_md = megeath_disks.iloc[idx[sep_constraint]]
+
+    # MA: Megeath All (disks + non)
+    matched_ma = onc_table[sep_constraint_a]
+    matches_ma = megeath_all_truncated.iloc[idx_a[sep_constraint]]
+
+    # MN: Megeath Nondisks
+    matched_mn = onc_table[sep_constraint_a & ~sep_constraint]
+
+    megeath_class = np.array(["--"] * len(onc_table))
+    for i in range(len(megeath_class)):
+        if sep_constraint[i]:
+            # NOT CORRECT but misses exactly one protostar.
+            megeath_class[i] = "D"
+        #         try:
+        #             megeath_class[i] = matches_md.loc[megeath_disks['Num'][idx[sep_constraint][i]]]['Class']
+        #         # edge case with I think two matches to the same Megeath target
+        #         except ValueError:
+        #             megeath_class[i] = matches_md.loc[megeath_disks['Num'][idx[sep_constraint][i]]]['Class'][0]
+        elif sep_constraint_a[i]:
+            megeath_class[i] = "ND"
+        else:
+            megeath_class[i] = "na"
+
+    onc_table["Class"] = megeath_class
+
+    irexc = np.select(megeath_class == [["D"], ["ND"], ["na"]], ["yes", "no", "--"])
+    onc_table["IRexc"] = irexc
+
+    return None
+
+    # return onc_match.approved
+
+
 onc_match = match_onc()
 ngc_match = match_ngc()
 ic_match = match_ic()
+
+for _attr in ("approved", "statistical"):
+    augment_oncmatch_with_disk_column(onc_match, _attr)
 
 reference_dict = {5: onc_match, 7: ngc_match, 8: ic_match}
 short_names = {5: "ONC", 7: "NGC", 8: "IC"}
@@ -148,6 +241,27 @@ def select_periodic_variables(wserv):
     return v_per
 
 
+def select_periodic_variables_experimental(wserv):
+
+    ds = spread[f"wserv{wserv}"]
+
+    flags = ["Y", "Yw", "N", "YfY", "?fY", "YfYw", "?fYw", "YfN", "?fN"]
+    periodic_flags = [flag for flag in flags if flag[-1] in ("Y", "w")]
+
+    period_sheet = pd.read_excel(
+        os.path.join(
+            period_directory,
+            f"{short_names[wserv]}_source_properties_periods_inspected.xlsx",
+        )
+    )
+    periodics = period_sheet[np.in1d(period_sheet["Periodic?"], periodic_flags)]
+    periodics_by_sourceid = periodics.set_index("SOURCEID")
+
+    #     v_per = np.in1d(ds.index, periodics.values)
+    #
+    return periodics_by_sourceid
+
+
 def select_subjective_variables(wserv):
 
     ds = spread[f"wserv{wserv}"]
@@ -209,22 +323,33 @@ if __name__ == "__main__":
         )
 
         print("")
-        if wserv != 5:
+        # if wserv != 5:
+        if True:
+            if wserv == 5:
+                for _attr in ("approved", "statistical"):
+                    augment_oncmatch_with_disk_column(onc_match, _attr)
 
             disks = select_disks(wserv)
-            nondisks = select_disks(wserv, choice='no')    
+            nondisks = select_disks(wserv, choice="no")
 
             rate_disks = np.sum(stat & vv & disks) / np.sum(stat & disks)
             per_rate_disks = np.sum(stat & v_per & disks) / np.sum(stat & disks)
             rate_nondisks = np.sum(stat & vv & nondisks) / np.sum(stat & nondisks)
-            per_rate_nondisks = np.sum(stat & v_per & nondisks) / np.sum(stat & nondisks)
-
+            per_rate_nondisks = np.sum(stat & v_per & nondisks) / np.sum(
+                stat & nondisks
+            )
 
             print(f"Disks: {np.sum(ref & disks)} (Stat: {np.sum(stat & disks)})")
-            print(f"Non-disks: {np.sum(ref & nondisks)} (Stat: {np.sum(stat & nondisks)})")
+            print(
+                f"Non-disks: {np.sum(ref & nondisks)} (Stat: {np.sum(stat & nondisks)})"
+            )
 
-            print(f"Variable Disks: {np.sum(ref & disks & vv)} (Stat: {np.sum(stat & disks & vv)})")
-            print(f"Variable Non-disks: {np.sum(ref & nondisks & vv)} (Stat: {np.sum(stat & nondisks & vv)})")
+            print(
+                f"Variable Disks: {np.sum(ref & disks & vv)} (Stat: {np.sum(stat & disks & vv)})"
+            )
+            print(
+                f"Variable Non-disks: {np.sum(ref & nondisks & vv)} (Stat: {np.sum(stat & nondisks & vv)})"
+            )
 
             print(
                 f"Statistical DISKED variability rate: {np.sum(stat & vv & disks)}/{np.sum(stat & disks)} = {rate_disks:.2f}"
@@ -238,6 +363,5 @@ if __name__ == "__main__":
             print(
                 f"Statistical NONDISKED periodicity rate: {np.sum(stat & v_per & nondisks)}/{np.sum(stat & nondisks)} = {per_rate_nondisks:.2f}"
             )
-
 
         print("\n")
