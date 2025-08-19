79000d4949adaec8767a1dfaa35d408d302659d2
Tom Rice <t.rice90@gmail.com>
2025-08-19 15:34:17 -0400

    mocking out some table/figure making code

---
+++ wuvars/publication_figures/make_figures.py
@@ -0,0 +1,20 @@
+"""
+This is a script designed for one thing: make all figures.
+
+The goal is for it to be up to date and ease my process.
+
+"""
+
+# Figure 1: 
+
+# Mass-SpT relationship
+
+
+# Figure 2:
+
+# The Map
+
+
+# Figure 3:
+
+# (What is it?)
