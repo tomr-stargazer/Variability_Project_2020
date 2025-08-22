4b18c257a3597af93aff143d7b2a07da59e79ad5
Tom Rice <t.rice90@gmail.com>
2025-08-22 11:18:17 -0400

    progress

--- wuvars/publication_figures/make_figures.py
+++ wuvars/publication_figures/make_figures.py
@@ -1,20 +1,34 @@
 """
 This is a script designed for one thing: make all figures.
 
-The goal is for it to be up to date and ease my process.
+The goal is for it to be up to date and ease my process. 
+Ideally: every publication figure (including appendices) is made here.
 
 """
 
-# Figure 1: 
 
-# Mass-SpT relationship
-
-
-# Figure 2:
-
-# The Map
+figure_export_path = "/Users/tsrice/Documents/Variability_Project_2020/wuvars/figs_tables_for_overleaf"
 
 
-# Figure 3:
+if __name__ == "__main__":
 
-# (What is it?)
+    # Figure 1:
+
+    # Mass-SpT relationship
+
+    from mass_SpT_figure import make_mass_SpT_figure
+
+    make_mass_SpT_figure()
+
+
+    # Figure 2:
+
+    # The Map
+
+    from map_figure_Perseus_regions_v4 import maps_v4_plus_WISEcolor
+
+    maps_v4_plus_WISEcolor()
+
+    # Figure 3:
+
+    # (What is it?)
