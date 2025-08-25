a153d8adb4719346b7a4bab18c09dd61e8b23290
Tom Rice <t.rice90@gmail.com>
2025-08-25 16:29:26 -0400

    not much here yet

---
+++ wuvars/tables/make_tables.py
@@ -0,0 +1,15 @@
+"""
+This is a script designed for one thing: make all tables.
+
+The goal is for it to be up to date and ease my process. 
+Ideally: 
+
+"""
+
+
+table_export_path = "/Users/tsrice/Documents/Variability_Project_2020/wuvars/figs_tables_for_overleaf"
+
+
+if __name__ == "__main__":
+
+    pass
