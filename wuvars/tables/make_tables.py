e522e45d48339bc1782c5f5addeb8f0026b3ba42
Tom Rice <t.rice90@gmail.com>
2025-10-22 15:32:06 -0400

    updates to make_tables

--- wuvars/tables/make_tables.py
+++ wuvars/tables/make_tables.py
@@ -7,9 +7,26 @@ Ideally:
 """
 
 
-table_export_path = "/Users/tsrice/Documents/Variability_Project_2020/wuvars/figs_tables_for_overleaf"
+table_export_path = (
+    "/Users/tsrice/Documents/Variability_Project_2020/wuvars/figs_tables_for_overleaf"
+)
 
 
 if __name__ == "__main__":
 
-    pass
+    # Make the first table, in some script somewhere
+    # We are going to (simultaneously in most cases)
+
+    # Table(s) 1:
+
+    from wuvars.tables.make_table_1_targets import make_table_1_targets
+
+    make_table_1_targets(verbose=False)
+
+    # Table(s) 2:
+
+    from wuvars.tables.make_table_2_variability_properties import (
+        make_table_2_variability_properties,
+    )
+
+    make_table_2_variability_properties(verbose=False)
