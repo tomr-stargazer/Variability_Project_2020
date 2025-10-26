c4c29f9ac529a19b7d8dc51bb271d3d62befef4f
Tom Rice <t.rice90@gmail.com>
2025-10-26 12:13:35 -0400

    make more robust to new software versions; produce publication-ready figure

--- wuvars/analysis/period_match.py
+++ wuvars/analysis/period_match.py
@@ -8,15 +8,18 @@ References the IPython notebook `Exploring literature periods.ipynb` a lot.
 
 """
 
+import os
 import pdb
 
 import astropy.table
 import astropy.units as u
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy.coordinates import SkyCoord
 from wuvars.analysis.load_periodics_v4 import (ic_periods, ngc_periods,
                                                select_periodic_variables_v4)
+from wuvars.publication_figures.make_figures import figure_export_path
 
 
 class LitPeriodTable(object):
@@ -159,7 +162,7 @@ fla1_pers = []
 
 for row in fla_1:
     if "LRLL " + str(row["LRLL"]) in fla_4["Star ID"]:
-        fla_4_match_index = np.int(np.where(row["LRLL"] == fla_4_lrll)[0])
+        fla_4_match_index = int(np.where(row["LRLL"] == fla_4_lrll)[0])
         fla1_pers.append(fla4_pers[fla_4_match_index].rstrip("^a"))
     else:
         fla1_pers.append(np.nan)
@@ -169,12 +172,19 @@ fla1_litpertable = LitPeriodTable(
     fla_1,
     fla_1["RAdeg"],
     fla_1["DEdeg"],
-    np.array(fla1_pers).astype(np.float),
+    np.array(fla1_pers).astype(float),
     short_name="F13",
     long_name="Flaherty et al. 2013",
 )
 
-if __name__ == "__main__":
+
+def make_period_comparison_figure(verbose=True, saving=False):
+
+    def vprint(*args, **kwargs):
+        if verbose:
+            print(*args, **kwargs)
+        else:
+            return None
 
     # testing this out on one case
 
@@ -200,233 +210,262 @@ if __name__ == "__main__":
     for _periods, _coords, _name, litpertable_list in zip(
         cluster_periods, cluster_coords, cluster_names, cluster_litpertable_lists
     ):
-        print(f"{_name}")
-        print(f"{np.sum(np.isfinite(_periods['Period']))} periodic objects")
-        print("")
+        vprint(f"{_name}")
+        vprint(f"{np.sum(np.isfinite(_periods['Period']))} periodic objects")
+        vprint("")
 
-        fig = plt.figure(figsize=(5, 5), dpi=150)
-        ax = fig.add_subplot(111)
-        ax.set_aspect("equal")
+        with mpl.rc_context(
+            {
+                "xtick.direction": "in",
+                "ytick.direction": "in",
+                "xtick.top": True,
+                "ytick.right": True,
+            }
+        ):
 
-        period_plots = []
+            fig = plt.figure(figsize=(5, 5), dpi=150)
+            ax = fig.add_subplot(111)
+            ax.set_aspect("equal")
 
-        for litpertable, symbol, ms in zip(litpertable_list, symbols, ms_list):
+            period_plots = []
 
-            idx, d2d, d3d = _coords.match_to_catalog_sky(litpertable.coords)
+            for litpertable, symbol, ms in zip(litpertable_list, symbols, ms_list):
 
-            max_sep = 1.0 * u.arcsec
-            sep_constraint = d2d < max_sep
+                idx, d2d, d3d = _coords.match_to_catalog_sky(litpertable.coords)
 
-            litpertable.idx = idx
-            litpertable.sep_constraint = sep_constraint
+                max_sep = 1.0 * u.arcsec
+                sep_constraint = d2d < max_sep
 
-            print(f"  Matches to {litpertable.long_name}: {np.sum(sep_constraint)}")
-            print(
-                f"    Of these, {np.sum(np.isfinite(litpertable.period[idx[sep_constraint]]))} were identified by {litpertable.short_name} as periodic"
+                litpertable.idx = idx
+                litpertable.sep_constraint = sep_constraint
+
+                vprint(f"  Matches to {litpertable.long_name}: {np.sum(sep_constraint)}")
+                vprint(
+                    f"    Of these, {np.sum(np.isfinite(litpertable.period[idx[sep_constraint]]))} were identified by {litpertable.short_name} as periodic"
+                )
+                vprint(f"  Closest match: {np.min(d2d).to(u.arcsec):.2f}")
+                vprint(
+                    f"  Farthest used match: {np.max(d2d[sep_constraint]).to(u.arcsec):.2f}"
+                )
+
+                try:
+                    litpertable.period = litpertable.period.filled(np.nan)
+                except AttributeError:
+                    pass
+
+                litpertable.overlaps = (
+                    np.isfinite(_periods["Period"][sep_constraint])
+                    & np.isfinite(litpertable.period[idx[sep_constraint]])
+                ) == True
+
+                litpertable.alt_overlaps = (
+                    np.isfinite(_periods["Period"])
+                    & np.isfinite(litpertable.period[idx])
+                    & sep_constraint
+                ) == True
+
+                litpertable.was_periodic = (
+                    np.isfinite(litpertable.period[idx]) & sep_constraint
+                ) == True
+
+                n_overlap = np.sum(litpertable.overlaps)
+                vprint(f"  Number of overlapping periods: {n_overlap}")
+
+                vprint("")
+
+                # Not sure I'm calculating this right.
+                # What exactly are we measuring?
+
+                # There are two things we might measure.
+
+                # One is the literal "how many periodic objects, total, did I find
+                # that are not literally listed as periodic in their catalog?"
+                # and this would be a sum of the following two groups:
+                #   (a) our-periodics that positionally matched to a non-periodic in their catalog;
+                #   (b) our-periodics that did not match to their catalog.
+
+                # It should literally be # of our found periods minus the number of periodic objects that matched to a periodic object
+
+                # Break it down a little more finely...
+                # a row in our table is flagged as a new period IF
+                #   (a) it has a finite period in our data AND EITHER
+                #      (b) it is not in the overlap period set among positional matches, OR
+                #      (c) it did not match to an object in their catalog at all
+
+                # I think I'm running into a challenge re: indexing on that latter bit.
+                # Because I want everything indexed to
+                our_new_periods = np.isfinite(_periods["Period"]) & (
+                    ~litpertable.alt_overlaps | ~sep_constraint
+                )
+                assert np.sum(our_new_periods) == np.sum(
+                    np.isfinite(_periods["Period"])
+                ) - np.sum(litpertable.overlaps)
+
+                our_new_periods_among_matches = np.isfinite(
+                    _periods["Period"][sep_constraint]
+                ) & (~litpertable.overlaps)
+
+                periods_we_missed = ~np.isfinite(
+                    _periods["Period"][sep_constraint]
+                ) & np.isfinite(litpertable.period[idx[sep_constraint]])
+
+                vprint(
+                    f"  Number of our new periods [relative to {litpertable.long_name}]: {np.sum(our_new_periods)}"
+                )
+                vprint(
+                    f"    Of which {np.sum(our_new_periods_among_matches)} were affirmatively not identified by {litpertable.short_name} as periodic"
+                )
+                vprint(
+                    f"  Number of periods we missed [relative to {litpertable.long_name}]: {np.sum(periods_we_missed)}"
+                )
+                vprint("")
+                vprint("")
+
+                matches = litpertable.table[idx[sep_constraint]]
+                litpertable.matches = matches
+
+                # pdb.set_trace()
+
+                period_plots.extend(
+                    ax.plot(
+                        _periods["Period"][sep_constraint],
+                        litpertable.period[idx[sep_constraint]],
+                        marker=symbol,
+                        linestyle="None",
+                        ms=ms,
+                        label=litpertable.long_name + f" (n={n_overlap})",
+                        zorder=20,
+                    )
+                )
+
+            ###
+            # Here's where it makes sense to do some math on the periodic recovery fraction / number of new periodics.
+
+            # total number of sources
+            vprint(
+                f"Total number of sources (periodic and not) in {_name}: {len(_periods)}"
             )
-            print(f"  Closest match: {np.min(d2d).to(u.arcsec):.2f}")
-            print(
-                f"  Farthest used match: {np.max(d2d[sep_constraint]).to(u.arcsec):.2f}"
+
+            is_periodic = np.isfinite(_periods["Period"])
+            n_is_periodic = np.sum(is_periodic)
+            vprint(f"Number of periodics: {n_is_periodic}")
+
+            overlaps_any = np.vstack(
+                [litpertable.alt_overlaps for litpertable in litpertable_list]
+            )
+            overlaps = np.any(overlaps_any, axis=0)
+
+            # was periodic in at least one of the literatures
+            # may require some kind of list comprehension over listpertable_list
+
+            was_periodic_any = np.vstack(
+                [litpertable.was_periodic for litpertable in litpertable_list]
+            )
+            was_periodic = np.any(was_periodic_any, axis=0)
+
+            vprint(
+                f"  Of these, {np.sum(overlaps)} were previously identified as periodic by {[x.short_name for x in litpertable_list]}"
             )
 
-            litpertable.overlaps = (
-                np.isfinite(_periods["Period"][sep_constraint])
-                & np.isfinite(litpertable.period[idx[sep_constraint]])
-            ) == True
+            new_periodics = np.isfinite(_periods["Period"]) & ~was_periodic
+            vprint("******** ********** ******** ****** ********")
+            vprint(f"Number of **new** periodics we found: {np.sum(new_periodics)}")
+            vprint("******** ********** ******** ****** ********")
 
-            litpertable.alt_overlaps = (
-                np.isfinite(_periods["Period"])
-                & np.isfinite(litpertable.period[idx])
-                & sep_constraint
-            ) == True
+            missed_periodics = ~np.isfinite(_periods["Period"]) & was_periodic
 
-            litpertable.was_periodic = (
-                np.isfinite(litpertable.period[idx]) & sep_constraint
-            ) == True
+            vprint("")
+            vprint(f"Number of known periodics we missed: {np.sum(missed_periodics)}")
 
-            n_overlap = np.sum(litpertable.overlaps)
-            print(f"  Number of overlapping periods: {n_overlap}")
-
-            print("")
-
-            # Not sure I'm calculating this right.
-            # What exactly are we measuring?
-
-            # There are two things we might measure.
-
-            # One is the literal "how many periodic objects, total, did I find
-            # that are not literally listed as periodic in their catalog?"
-            # and this would be a sum of the following two groups:
-            #   (a) our-periodics that positionally matched to a non-periodic in their catalog;
-            #   (b) our-periodics that did not match to their catalog.
-
-            # It should literally be # of our found periods minus the number of periodic objects that matched to a periodic object
-
-            # Break it down a little more finely...
-            # a row in our table is flagged as a new period IF
-            #   (a) it has a finite period in our data AND EITHER
-            #      (b) it is not in the overlap period set among positional matches, OR
-            #      (c) it did not match to an object in their catalog at all
-
-            # I think I'm running into a challenge re: indexing on that latter bit.
-            # Because I want everything indexed to
-            our_new_periods = np.isfinite(_periods["Period"]) & (
-                ~litpertable.alt_overlaps | ~sep_constraint
-            )
-            assert np.sum(our_new_periods) == np.sum(
-                np.isfinite(_periods["Period"])
-            ) - np.sum(litpertable.overlaps)
-
-            our_new_periods_among_matches = np.isfinite(
-                _periods["Period"][sep_constraint]
-            ) & (~litpertable.overlaps)
-
-            periods_we_missed = ~np.isfinite(
-                _periods["Period"][sep_constraint]
-            ) & np.isfinite(litpertable.period[idx[sep_constraint]])
-
-            print(
-                f"  Number of our new periods [relative to {litpertable.long_name}]: {np.sum(our_new_periods)}"
-            )
-            print(
-                f"    Of which {np.sum(our_new_periods_among_matches)} were affirmatively not identified by {litpertable.short_name} as periodic"
-            )
-            print(
-                f"  Number of periods we missed [relative to {litpertable.long_name}]: {np.sum(periods_we_missed)}"
-            )
-            print("")
-            print("")
-
-            matches = litpertable.table[idx[sep_constraint]]
-            litpertable.matches = matches
+            vprint("")
+            vprint("")
 
             # pdb.set_trace()
 
-            period_plots.extend(
+            # Create a legend for the first line.
+            first_legend = ax.legend(
+                handles=period_plots,
+                loc="upper left",
+                title=f"{_name} period comparisons\n",
+            )
+
+            # Add the legend manually to the Axes.
+            ax.add_artist(first_legend)
+
+            alias_plots = []
+
+            alias_plots.extend(
                 ax.plot(
-                    _periods["Period"][sep_constraint],
-                    litpertable.period[idx[sep_constraint]],
-                    marker=symbol,
-                    linestyle="None",
-                    ms=ms,
-                    label=litpertable.long_name + f" (n={n_overlap})",
-                    zorder=20,
+                    [0.01, 100],
+                    [0.01, 100],
+                    "k--",
+                    lw=0.25,
+                    label="$y=x$",
+                )
+            )
+            alias_plots.extend(
+                ax.plot(
+                    [0.01, 100],
+                    [0.01 * 2, 100 * 2],
+                    "k--",
+                    lw=0.1,
+                    label="$y=2x$, $y=x/2$",
+                )
+            )
+            ax.plot([0.01, 100], [0.01 / 2, 100 / 2], "k--", lw=0.1)
+
+            # do the hyperbolic ones
+            xs_a = np.linspace(1.01, 20, 200)
+            xs_b = np.linspace(0.01, 20, 200)
+            xs_c = np.linspace(0.01, 0.99, 50)
+            ys1_inv = 1 - 1 / xs_a
+            ys2_inv = 1 + 1 / xs_b
+            ys3_inv = 1 / xs_c - 1
+
+            alias_plots.extend(
+                ax.plot(
+                    xs_a,
+                    1 / ys1_inv,
+                    "k:",
+                    lw=0.1,
+                    label=r"$y = \rm{abs} \left( \pm \left( \frac{1}{x} \pm 1 \right) \right ) ^{-1}$",
                 )
             )
 
-        ###
-        # Here's where it makes sense to do some math on the periodic recovery fraction / number of new periodics.
-
-        # total number of sources
-        print(f"Total number of sources (periodic and not) in {_name}: {len(_periods)}")
-
-        is_periodic = np.isfinite(_periods["Period"])
-        n_is_periodic = np.sum(is_periodic)
-        print(f"Number of periodics: {n_is_periodic}")
-
-        overlaps_any = np.vstack(
-            [litpertable.alt_overlaps for litpertable in litpertable_list]
-        )
-        overlaps = np.any(overlaps_any, axis=0)
-
-        # was periodic in at least one of the literatures
-        # may require some kind of list comprehension over listpertable_list
-
-        was_periodic_any = np.vstack(
-            [litpertable.was_periodic for litpertable in litpertable_list]
-        )
-        was_periodic = np.any(was_periodic_any, axis=0)
-
-        print(
-            f"  Of these, {np.sum(overlaps)} were previously identified as periodic by {[x.short_name for x in litpertable_list]}"
-        )
-
-        new_periodics = np.isfinite(_periods["Period"]) & ~was_periodic
-        print("******** ********** ******** ****** ********")
-        print(f"Number of **new** periodics we found: {np.sum(new_periodics)}")
-        print("******** ********** ******** ****** ********")
-
-        missed_periodics = ~np.isfinite(_periods["Period"]) & was_periodic
-
-        print("")
-        print(f"Number of known periodics we missed: {np.sum(missed_periodics)}")
-
-        print("")
-        print("")
-
-        # pdb.set_trace()
-
-        # Create a legend for the first line.
-        first_legend = ax.legend(handles=period_plots, loc="upper left")
-
-        # Add the legend manually to the Axes.
-        ax.add_artist(first_legend)
-
-        alias_plots = []
-
-        alias_plots.extend(
-            ax.plot(
-                [0.01, 100],
-                [0.01, 100],
-                "k--",
-                lw=0.25,
-                label="$y=x$",
-            )
-        )
-        alias_plots.extend(
-            ax.plot(
-                [0.01, 100],
-                [0.01 * 2, 100 * 2],
-                "k--",
-                lw=0.1,
-                label="$y=2x$, $y=x/2$",
-            )
-        )
-        alias_plots.extend(ax.plot([0.01, 100], [0.01 / 2, 100 / 2], "k--", lw=0.1))
-
-        # do the hyperbolic ones
-        xs_a = np.linspace(1.01, 20, 200)
-        xs_b = np.linspace(0.01, 20, 200)
-        xs_c = np.linspace(0.01, 0.99, 50)
-        ys1_inv = 1 - 1 / xs_a
-        ys2_inv = 1 + 1 / xs_b
-        ys3_inv = 1 / xs_c - 1
-
-        alias_plots.extend(
-            ax.plot(
-                xs_a,
-                1 / ys1_inv,
-                "k:",
-                lw=0.1,
-                label=r"$y = \rm{abs} \left( \pm \left( \frac{1}{x} \pm 1 \right) \right ) ^{-1}$",
-            )
-        )
-        alias_plots.extend(
             ax.plot(
                 xs_b,
                 1 / ys2_inv,
                 "k:",
                 lw=0.1,
             )
-        )
-        alias_plots.extend(
+
             ax.plot(
                 xs_c,
                 1 / ys3_inv,
                 "k:",
                 lw=0.1,
             )
-        )
 
-        ax.axvline(1, color="k", linestyle=":", lw=0.25)
-        ax.axhline(1, color="k", linestyle=":", lw=0.25)
+            ax.axvline(1, color="k", linestyle=":", lw=0.25)
+            ax.axhline(1, color="k", linestyle=":", lw=0.25)
 
-        ax.set_xlim(0, 17)
-        ax.set_ylim(0, 17)
+            ax.set_xlim(0, 17)
+            ax.set_ylim(0, 17)
 
-        ax.set_xlabel("Our period (d)")
-        ax.set_ylabel("Literature period (d)")
+            ax.set_xlabel("Our period (d)")
+            ax.set_ylabel("Literature period (d)")
 
-        ax.set_title(f"Literature period comparisons in {_name}")
+            # ax.set_title()
 
-        ax.legend(handles=alias_plots, loc="lower right")
+            ax.legend(handles=alias_plots, loc="lower right")
+
+            if saving:
+                filename = os.path.join(figure_export_path, f"Figure6_{_name.split()[0]}")
+                fig.savefig(filename+".png", bbox_inches='tight')
+                fig.savefig(filename+".pdf", bbox_inches='tight')
+                plt.close()
+
+
+if __name__ == "__main__":
+
+    make_period_comparison_figure(verbose=False)
